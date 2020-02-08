7592e662a1b5b771aee7931835c770ffb3b57d51
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-08 06:25:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0){
+    return 'Your shopping cart is empty.'
+  }
+  else if (cart.length === 1){
     return ''
   }
   let message = 'In your cart, you have'
